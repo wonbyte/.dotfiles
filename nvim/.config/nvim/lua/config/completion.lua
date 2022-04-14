6685ceddd1151ae9804d948ac165a0f8fc0dd4c5
Gregory Sieranski <git@wonbyte.com>
2022-04-14 12:51:53 -0500

    update per cmp docs

--- nvim/.config/nvim/lua/config/completion.lua
+++ nvim/.config/nvim/lua/config/completion.lua
@@ -61,27 +61,13 @@ cmp.setup({
       return vim_item
     end,
   },
-  mapping = {
+  mapping = cmp.mapping.preset.insert({
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif vim.fn["vsnip#available"](1) == 1 then
-        feedkey("<Plug>(vsnip-expand-or-jump)", "")
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function()
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-        feedkey("<Plug>(vsnip-jump-prev)", "")
-      end
-    end, { "i", "s" }),
-  },
+  }),
   experimental = {
     ghost_text = true,
   },
