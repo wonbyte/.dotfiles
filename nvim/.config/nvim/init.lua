1116922c8793cc9f5e2230cc3af280564179638a
Gregory Sieranski <git@wonbyte.com>
2022-06-28 14:23:00 -0400

    make sure lsp servers are installed

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -8,7 +8,15 @@ require("keymappings")
 require("plugins")
 
 -- lsp install (TODO: figure out why this doesn't work in packer `config`)
-require("nvim-lsp-installer").setup {}
+require("nvim-lsp-installer").setup({
+  ensure_installed = {
+    "diagnosticls",
+    "eslint",
+    "rust_analyzer",
+    "sumneko_lua",
+    "tsserver",
+  },
+})
 
 -- lsp setup
 require("lsp")
