466b7a8df49a953e73dd339b764f4d64242e1168
Gregory Sieranski <git@wonbyte.com>
2022-06-28 11:30:21 -0400

    go back to always showing diagnostic messages

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -77,16 +77,6 @@ require("lspconfig").sumneko_lua.setup({
 require("lspconfig").rust_analyzer.setup({
   on_attach = on_attach,
   capabilities = capabilities,
-  handlers = {
-    ["textDocument/publishDiagnostics"] = vim.lsp.with(
-      vim.lsp.diagnostic.on_publish_diagnostics,
-      {
-        -- Disable virtual_text
-        virtual_text = false,
-        severity_sort = true,
-      }
-    ),
-  },
 })
 
 -- js/ts
