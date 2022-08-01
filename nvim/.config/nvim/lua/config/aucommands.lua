bedd2ac523b7e0d704ddec1775d02661b154a012
Gregory Sieranski <git@wonbyte.com>
2022-08-01 08:35:52 -0400

    fix filetype commands

--- nvim/.config/nvim/lua/config/aucommands.lua
+++ nvim/.config/nvim/lua/config/aucommands.lua
@@ -4,38 +4,42 @@ local create_augroup = vim.api.nvim_create_augroup
 create_augroup("Filetypes", { clear = false })
 create_augroup("Highlights", { clear = true })
 
-autocmd({ "FileType javascript" }, {
+autocmd({ "FileType" }, {
   desc = "set spacing for javascript",
+  pattern = "javascript",
   command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
   group = "Filetypes",
 })
 
-autocmd({ "FileType javascriptreact" }, {
+autocmd({ "FileType" }, {
   desc = "set spacing for javascriptreact",
+  pattern = "javascriptreact",
   command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
   group = "Filetypes",
 })
 
-autocmd({ "FileType typescript" }, {
+autocmd({ "FileType" }, {
   desc = "set spacing for typescript",
+  pattern = "typescript",
   command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
   group = "Filetypes",
 })
 
 autocmd({ "FileType typescriptreact" }, {
   desc = "set spacing for typescriptreact",
+  pattern = "typescriptreact",
   command = "setlocal tabstop=2 softtabstop=2 shiftwidth=2",
   group = "Filetypes",
 })
 
 -- highlight on yank
 autocmd({ "TextYankPost" }, {
+  group = "Highlights",
+  pattern = "*",
   callback = function()
     vim.highlight.on_yank({
       higroup = "IncSearch",
       timeout = 40,
     })
   end,
-  group = "Highlights",
-  pattern = "*",
 })
